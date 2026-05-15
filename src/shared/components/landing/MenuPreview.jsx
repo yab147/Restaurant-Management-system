b15bf70eb6fa0d8a1209b92832e9c05645892a9e
yab147 <yeabsiraeskinder859@gmai.com>
2026-05-15 21:03:04 +0300

    updated broken import path

--- src/shared/components/landing/MenuPreview.jsx
+++ src/shared/components/landing/MenuPreview.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useApp } from '../../context/AppContext';
+import { useApp } from '../../../context/AppContext';
 
 const MenuPreview = () => {
   const { menuItems, menuCategories } = useApp();
